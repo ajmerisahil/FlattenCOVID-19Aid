c4193c5e855b632580b971c5f87c3a5994ffaa8d
Sahil Ajmeri <sahil.ms1113@gmail.com>
2021-03-26 10:25:39 +0530

    Contact page connected with backend

--- fca/shop/views.py
+++ fca/shop/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Contact
+
 # Create your views here.
 
 def index(request):
@@ -15,6 +17,14 @@ def comingsoon(request):
     return render(request,'shop/comingsoon.html')
 
 def contact(request):
+    if request.method=="POST":
+        name  = request.POST.get('name' , '')
+        email = request.POST.get('email' , '')
+        phone = request.POST.get('phone' , '')
+        desc  = request.POST.get('desc' , '')
+        print( name , email , phone , desc)
+        contact = Contact(name=name,email=email,phone=phone,desc=desc)
+        contact.save()
     return render(request , 'shop/contact.html')
 
 def doctors(request):
